8af87ec8e12f9945a6553af48c21b75bfacc1c0a
FeridBiberoglu <biberoglu.feriddeniz@gmail.com>
2024-10-17 18:35:32 +0200

    s

--- app/config.py
+++ app/config.py
@@ -1,16 +1,25 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-MONGODB_URL = os.getenv("MONGODB_URL")
-MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "profitsniffer")
+# MONGODB_URL = os.getenv("MONGODB_URL")
+# MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "profitsniffer")
+
+# class Config:
+#     MONGODB_URL = MONGODB_URL
+#     MONGODB_DB_NAME = MONGODB_DB_NAME
+#     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+#     FRONTEND_URL = os.getenv("FRONTEND_URL")
+#     SCRAPER_URL = os.getenv("SCRAPER_URL", "http://host.docker.internal:3000/cf-clearance-scraper")
+
+# config = Config()
 
 class Config:
-    MONGODB_URL = MONGODB_URL
-    MONGODB_DB_NAME = MONGODB_DB_NAME
-    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    FRONTEND_URL = os.getenv("FRONTEND_URL")
-    SCRAPER_URL = os.getenv("SCRAPER_URL", "http://host.docker.internal:3000/cf-clearance-scraper")
+    MONGODB_DB_NAME = "profitsniffer"
+    MONGODB_URL = "mongodb+srv://feridbiberoglu88:Amsterdam1@profitsniffer.vxoqb.mongodb.net/"
+    TELEGRAM_BOT_TOKEN = "7824046971:AAGGdnb1V-0SVau6TRAdCsnkw0_hQd7gl9c"
+    FRONTEND_URL = "https://elegant-duckanoo-d8e6c0.netlify.app"
+    SCRAPER_URL = "http://host.docker.internal:3000/cf-clearance-scraper"
 
 config = Config()
